f06a440cd6db0806cc16d805771817a897b075d4
Leandro Ostera <leandro@ostera.io>
2016-10-31 08:30:37 +0100

    🏹

--- src/app.js
+++ src/app.js
@@ -1,43 +1,16 @@
-// Base Combinators
-const apply  = f => s => f(s)
-const filter = (f, c) => s => f(s) && c(s)
-const id     = a => a
-const map    = (f, c) => s => c(f(s))
+//@flow
 
-// Side-effectful Combinators
-const log = (prefix, combinator) => s => {
-  console.log(performance.now()|0, prefix, "=>", s)
-  return combinator(s)
+import { log } from './log'
+
+type Arrow = {
+  id(): Function;
+  next(g: Function): Function;
 }
 
-// Some Mappers/Filters
-const has    = name => o => o[name] !== undefined
-const to     = name => o => o[name]
-const exists = o => !!o
-const odd    = a => !!(a%2)
-
-// Streams
-
-/* more realistic app state
-const tap = (f, c) => s => {
-  f(s)
-  return c
+// Lifts a function into an Arrow
+// arrrow :: (b -> c) -> Arrow b c
+const arrow = (f: Function): Arrow  => {
+  f.id   = () => f
+  f. = g => x => g(f(x))
+  return f
 }
-
-let prevState
-const saveStore = (nextState) => {
-  prevState !== nextState && push(nextState)
-  prevState = nextState
-}
-
-const listeners = compose(
-    filter((listener) => listener != null),
-    map((effect) => effect(push)),
-)(effects)
-
-const push =
-  scan(reducer, initialState,
-    tap(saveStore,
-      map(applyListener,
-        render)))
-*/
