bff23903fb9556261e2ca36928dccaf1bd092885
Leandro Ostera <leandro@ostera.io>
2016-11-08 21:38:47 +0100

    Actually Streams...

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import kalachakra from 'kalachakra'
+
 import {
   tick as now,
   log,
   atom,
 } from 'zazen/utils'
+
+log(kalachakra)
 
 type Pair<T>  = [ T, T ]
 type Arrow = {
@@ -16,41 +20,24 @@ type Arrow = {
 // Lifts a function into an Arrow
 // arrrow :: (b -> c) -> Arrow b c
 const arrow = (f: Function): Arrow  => {
-  const first  = () => arrow( ([a, _]: Pair): Pair => [f(a), _] )
-  const second = () => arrow( ([_, b]: Pair): Pair => [_, f(b)] )
+  f.first  = () => arrow( ([a, _]: Pair): Pair => [f(a), _] )
+  f.second = () => arrow( ([_, b]: Pair): Pair => [_, f(b)] )
 
   const compose = (g) => arrow( x => f(g(x)) )
   const combine = (g) => arrow( ([a, b]) => [f(a),g(b)] )
-  const fanout  = (g) => combine(g).compose( arrow( x => [x,x] ) )
-
-  f.first  = first
-  f.second = second
 
   f.compose = compose
   f.combine = combine
-  f.fanout  = fanout
+  f.fanout  = (g) => combine(g).compose( arrow( x => [x,x] ) )
 
   return f
-}
-
-type Stream = Arrow & {
-  (a: mixed[]): mixed[];
 }
 
 // Lifts a function into a Stream Arrow
 // stream :: (b -> c) -> Arrow [b] [c]
-const stream = (m: Function): Stream => {
-  const f = a => a.map(m)
+const stream = (f: Function): Arrow => arrow( a => a.map(f) )
 
-  const compose = (g) => arrow( x => f(g(x)) )
-
-  f.first  = () => arrow( ([a, _]: Pair): Pair => [f(a), _] )
-  f.second = () => arrow( ([_, b]: Pair): Pair => [_, f(b)] )
-
-  f.compose = compose
-
-  return f
-}
+export { arrow, stream }
 
 window.arrow = arrow
 window.stream = stream
