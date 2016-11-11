72e6d76c8fe500bf1d61edd16cbac197d3ad13d0
Leandro Ostera <leandro@ostera.io>
2016-11-11 11:58:42 +0100

    Revert "Revert "Revert "Experiment with abstracting away arrow composition"""
    This reverts commit 13f669b3eb97450a538c2d6f5fb8b1654eb91acb.

--- src/index.js
+++ src/index.js
@@ -29,50 +29,11 @@ const arrow = (f: Function): Arrow  => {
 // stream :: (b -> c) -> Arrow [b] [c]
 const stream = (f: Function): Arrow => (arrow( a => a.map(f) ))
 
-
-const to_coords = ({x,y}) => ({x,y})
-const to_key = ({key}) => key
-const is_trusted = ({isTrusted}) => isTrusted
-
-// Event -> Coords
-let trusted = arrow(is_trusted)
-// Event -> Coords
-let clicks = arrow(to_coords)
-// Event -> KeyCode
-let keys   = arrow(to_key)
-// (Event, Event) -> (Coords, KeyCode)
-let inputs = arrow( x => x )
-  .combine(keys)
-  .combine(clicks)
-  .combine(trusted)
-  .compose( x => [ [ [x,x], x], x ] )
-//  [a,b] => [ f(a), keys(b) ]
-//  [ [a,b], c ] => [ [f(a), keys(b)], clicks(c) ]
-//  [ [[a,b],c], d ] => [ [ [f(a), keys(b)], clicks(c) ], trusted(d) ]
-//  x => [ [ [f(x), keys(x)], clicks(x) ], trusted(x) ]
-//
-const flatten = (list, depth) => {
-  return depth === 0 ?
-    list :
-    flatten(list.reduce( (acc,e) => acc.concat(e), [] ), depth-1)
-}
-const nested_pairs = (n) => (x) => (n===0) ? x : [].concat([nested_pairs(n-1)(x), x])
-const arrowify = (combinators) => {
-  let a = combinators.reduce( (arr, c) => {
-    return arr.combine(c)
-  }, arrow( x => x ))
-  return arrow( x => x )
-    .compose(arrow( x => flatten(x, combinators.length) )
-      .compose(a
-        .compose(nested_pairs(combinators.length))))
-}
-let inputsify =
-  arrow(log.ns("Inputsify"))
-    .compose(arrowify( [keys, clicks, trusted ] ))
-
-document.addEventListener('mousedown', inputsify)
-document.addEventListener('keydown', inputsify)
-
 let push = stream( x => x )
 
 export { arrow, stream }
+
+window.arrow = arrow
+window.stream = stream
+
+window.push = push
