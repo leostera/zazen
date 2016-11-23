0aaec0c10fb8f14415f866062464e7c6dd831997
Leandro Ostera <leandro@ostera.io>
2016-11-23 23:36:57 +0100

    Adds combination koan

--- koans/1.intro.js
+++ koans/1.intro.js
@@ -28,37 +28,56 @@ const mul3 = x => x*3
  *
  */
 
-koan('an arrow is just a function', ({ok, end}) => {
+koan(`an arrow is just a function`, ({ok, end}) => {
 
   const arr = arrow(add1)
 
-  ok('function' == typeof ___,
-    'the type of an arrow is function')
+  ok(`function` == typeof ___,
+    `the type of an arrow is function`)
 
   ok(2 == ___,
-    'we can invoke an arrow')
+    `we can invoke an arrow`)
 
   end()
 
 })
 
-koan('an arrow can be composed with another arrow', ({ok, end}) => {
+koan(`an arrow can be composed with another arrow`, ({ok, end}) => {
 
   const arr1 = arrow(add1)
 
   const arr3 = arrow(mul3)
 
   ok(4 == arr1.___(arr3)(1),
-    "the output of arr3 goes into arr1")
+    `the output of arr3 goes into arr1`)
 
   ok(6 == arr1.___(arr3)(1),
-    "the output of arr1 goes into arr3")
+    `the output of arr1 goes into arr3`)
 
   ok(6 == arr3.___(arr1)(1),
-    "the output of arr1 goes into arr3")
+    `the output of arr1 goes into arr3`)
 
   ok(4 == arr3.__(arr1)(1),
-    "the output of arr3 goes into arr1")
+    `the output of arr3 goes into arr1`)
+
+  end()
+
+})
+
+koan(`an arrow can be combined with another arrow`, ({deepEquals, end}) => {
+
+  const arr1 = arrow(add1)
+
+  const arr3 = arrow(mul3)
+
+  deepEquals(
+    [2, 3], ___.combine(___)([1, 1]),
+    `Left input goes through left arrow to left output,
+     right input goes through right arrow to right output`)
+
+  deepEquals(
+    [2, 3], arr1.___(arr3)(1),
+    `Input goes through both arrows`)
 
   end()
 
