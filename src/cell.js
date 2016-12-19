746fb0c71792c6604c82a5cc158b3ed7d18cb1b2
Leandro Ostera <leandro@ostera.io>
2016-12-19 22:53:56 +0100

    Cells that work

---
+++ src/cell.js
@@ -0,0 +1,35 @@
+import {
+  arrow,
+} from 'zazen'
+
+const dirty = () => true
+
+const cell = (fn, child) => {
+  const cell_fn = arrow((...args) => {
+    if(cell_fn.last_args[0] !== args[0]) {
+      console.log(`Recalculating cell with ${args}`)
+      cell_fn.last_args = args
+      cell_fn.last_val = fn.apply({}, args)
+      child(cell_fn.last_val)
+    }
+    return cell_fn.last_val
+  })
+
+  cell_fn.last_args = []
+  cell_fn.last_val = undefined
+  cell_fn.dirty = true
+
+  return cell_fn
+}
+
+const adder = n => x => {
+  const y = x+n
+  console.log(`adder(${n})(${x}) -> ${y}`)
+  return y
+}
+
+cell( adder(1) ,
+  cell( adder(2),
+    cell(
+      cell( adder(3), adder(4) ),
+        adder(10) )))(1)
