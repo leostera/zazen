faa05b28a36893439d300fdd3e1caba8a45fdb48
Leandro Ostera <leandro@ostera.io>
2017-01-16 23:14:15 +0100

    Rollback constructors for Left/Right.
    After several attempts, I can't yet manage to make
    an Either constructor that type checks for Left/Right
    as the string of the Atom, and extends the pair-array
    with an inspect method that both shows a nicer representation
    of the type and allows for equality checks between
    them.

--- src/either.js
+++ src/either.js
@@ -6,35 +6,19 @@ import {
   atom,
 } from 'zazen/utils'
 
-type Pair<A, B> = [A, B]
-
-type LeftStr = "Left"
-type RightStr = "Right"
 export type Left  = Atom
 export type Right = Atom
 export type Either = [ Left | Right, mixed ]
 
-const build_either = (t: LeftStr | RightStr, a: mixed): Either => {
-  const e = [ atom(t), a ]
-  // Okay, so __proto__ doesn't work, and neither does Symbol.toStringTag
-  // need to figure this out without having to use `new` everywhere
-  e.toString = () => {
-    console.log(this)
-    return `${t}(${Object.prototype.toString.call(a)})`
-  }
-  return e
-}
-
-const left  = (a: mixed): Either => build_either('Left', a)
-const right = (a: mixed): Either => build_either('Right', a)
+const left  = (a: mixed): Either => [atom('Left'), a]
+const right = (a: mixed): Either => [atom('Right'), a]
 
 const either = (f: Function) => (g: Function) => ([tag, a]: Either): ?Either => {
   if( tag == atom('Left')  ) return left(f(a))
-  // Need to figure out this shitty ?Either thing
   if( tag == atom('Right') ) return right(g(a))
 }
 
-const mirror = ([tag, a]: Either): Either => {
+const mirror = ([tag, a]: Either): ?Either => {
   if( tag == atom('Left')  ) return right(a)
   if( tag == atom('Right') ) return left(a)
 }
