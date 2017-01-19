67f05019d222c9e9729446f1dcedea862dbbdb5b
Leandro Ostera <leandro@ostera.io>
2017-01-19 17:49:04 +0100

    Test cell 📱

---
+++ src/__tests__/cell.test.js
@@ -0,0 +1,59 @@
+import {
+  check as verify,
+  forall,
+  nat,
+} from 'jsverify'
+
+import {
+  log,
+  atom,
+} from 'zazen/utils'
+
+import {
+  cell,
+} from 'zazen'
+
+const options = {
+  quiet: true,
+  tests: 1000,
+}
+
+const id   = x => x
+const str  = JSON.stringify
+const add1 = x => x + 1
+
+test(`a Cell is just a function with dependants`, () => {
+  const c = cell( add1, id )
+
+  expect( typeof c ).toEqual('function')
+  expect( c(1) ).toEqual(2)
+})
+
+test(`a Cell will pass it's value on to it's dependants`, () => {
+  const c = cell( add1, x => expect(x).toEqual(2) )
+  c(1)
+})
+
+test(`a Cell's child can be another cell`, () => {
+  const c =
+  cell( add1,
+    cell( add1,
+      cell( add1,
+        cell( add1,
+          x => expect(x).toEqual(5)))))
+  c(1)
+})
+
+test(`a Cell will only recompute on new parameters`, () => {
+  let a = 0
+  const c = cell( add1, () => a++ )
+
+  expect( a ).toEqual(0)
+  c(1)
+  expect( a ).toEqual(1)
+  c(1)
+  c(1)
+  expect( a ).toEqual(1)
+  c(22)
+  expect( a ).toEqual(2)
+})
