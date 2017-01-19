ba94a25c44be40e9b199e1a02417dabd1a1633be
Leandro Ostera <leandro@ostera.io>
2017-01-19 14:41:01 +0100

    Type annotate Cell

--- src/cell.js
+++ src/cell.js
@@ -1,10 +1,21 @@
+import type {
+  Arrow,
+} from 'zazen/arrow'
+
 import {
   arrow as arr,
 } from 'zazen'
 
 const dirty = (oldArgs, newArgs) => true
 
-const cell = (fn, child) => {
+export type Cell = Arrow & {
+  last_args: [];
+  last_val: ?mixed;
+  dirty: Boolean;
+}
+
+export type CellFn = (fn: Function, child: Cell) => Cell
+const cell: CellFn = (fn, child) => {
   const cell_fn = arr((...args) => {
     const arg_s = `(${args.join(', ')})`
     if(cell_fn.last_args[0] !== args[0]) {
@@ -21,37 +32,6 @@ const cell = (fn, child) => {
 
   return cell_fn
 }
-
-
-// const effect = (effect) => {
-//   let theNext = () => {}
-//   const listener = effect(theNext)
-//
-//   return ({next, state}) => {
-//     theNext = next
-//     return listener(state)
-//   }
-// }
-
-const effect = (f) => {
-  let state = { }
-  const g = f(state)
-
-  return cell( x => x )
-    .loop(state, (...args) => (g.apply({}, args)) )
-}
-
-effect(
-  (next) => {
-    const onClick = () => next({type: 'CLICKED'})
-
-    return cell((state) => ({
-      type: 'div',
-      props: [{name: 'onClick', value: onClick}],
-      children: []
-    }), x => x)
-  }
-)
 
 export {
   cell,
