0ab8758f1ca428acb6717adce9b54b1b8de98d4a
Xavier Via <fernando.via@gmail.com>
2017-01-29 20:41:19 +0100

    Adapt Cell a little more

--- src/cell.js
+++ src/cell.js
@@ -1,9 +1,9 @@
 // let f
-// 
+//
 // (a, b)
 // with b = (1, (2, (3, 4)))
-// 
-// 
+//
+//
 // f(a) === first(b) => Left (a, b)
 //                   => Right(a, (first(b), g(f(a), second(b))))
 //
@@ -46,28 +46,38 @@ const Cell: CellFn = f => {
       [true, Right([fx, t])])
   }
 
-  _c.compose = g => 
+  // first comes from product
+  // second comes from first
 
-  _c.pipe = g => (Cell(id).sum(g)).compose(_c)
+  _c.compose = g => _c.compose(g.sum(Cell(id)))
+  // pipe comes from compose
+
+  _c.product = g => Cell( pair(f)(g) )
+  // fanout comes from product
 
   _c.sum = g => Cell( ([t,a]) =>
     cond(
       [eq(t, 'Left'),  Left(f(a))],
       [eq(t, 'Right'), Right(g(a))]))
 
+  // fanin comes from sum and pipe
+
+  // left comes from sum
+  _c.right = x => Cell(id).sum(f)(x)
+
   return _c
 }
 
-const add1Cell = Cell( x => x + 1 )
-const mul2Cell = Cell( y => y * 2 )
-
-const add1ThenMul2Cell = add1Cell.pipe(mul2Cell)
-
-const add1Values = add1Cell(1, [1, 2]) // => Left([1, 2])
-const add1NewValues = add1Cell(2, [1, 2]) // => Right([2, 3])
-
-const values = add1ThenMul2Cell(1, [1, [2, 4]]) // Left([1, [2, 4]])
-const newValues = add1ThenMul2Cell(1, [2, [4, 6]]) // Right([2, Right([3, 6])])
+// const add1Cell = Cell( x => x + 1 )
+// const mul2Cell = Cell( y => y * 2 )
+//
+// const add1ThenMul2Cell = add1Cell.pipe(mul2Cell)
+//
+// const add1Values = add1Cell(1, [1, 2]) // => Left([1, 2])
+// const add1NewValues = add1Cell(2, [1, 2]) // => Right([2, 3])
+//
+// const values = add1ThenMul2Cell(1, [1, [2, 4]]) // Left([1, [2, 4]])
+// const newValues = add1ThenMul2Cell(1, [2, [4, 6]]) // Right([2, Right([3, 6])])
 
 export {
   Cell,
