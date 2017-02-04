9ba390edc27858c75e75c7f940ff69a619b43efc
Leandro Ostera <leandro@ostera.io>
2017-02-04 22:35:59 +0100

    Better Type Checking

--- src/type.js
+++ src/type.js
@@ -1,14 +1,36 @@
-const createType = name => {
-  const of = x => {
-    const element = [name, x]
-    element.fold = f => f(x)
-    element.inspect = () => `${name}(${x})`
-    element.map = f => Object.freeze(of(f(x)))
-    return Object.freeze(element)
-  }
+/*
+ * Generic TypeChecker used to coerce inputs of functions when using them.
+ * See: https://github.com/ostera/zazen/pull/20
+ */
+export type TypeChecker<A> = (a: A) => A
 
-  return Object.freeze({of})
+/*
+ * Generic TypeRepresentation Type, used to ensure that types have the right
+ * internal structure.
+ */
+export type Type<A, B> = {
+  '@@type': A,
+  '@@value': B
 }
+
+/*
+ * Generic Data Type, used to ensure that all lifters can be properly
+ * type-checked.
+ */
+export type Data<A, B> = {
+  of: (x: B) => A
+}
+
+/*
+ * Generic Type Creator. If used with Flow's Inference engine, works smoothly
+ * to verify that `of` blows up in time for the wrong types.
+ */
+const createType = (name: any): any => ({
+  of: x => ({
+    '@@type': name,
+    '@@value': x,
+  })
+})
 
 export {
   createType,
