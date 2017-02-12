9dbb0edfc386be6729e4e320f3edf84357a81a3a
Xavier Via <fernando.via@gmail.com>
2017-02-12 20:22:10 +0100

    Some cleanup

--- src/cell.js
+++ src/cell.js
@@ -38,18 +38,6 @@ const Cell = (f, next) => {
   return c
 }
 
-// Cell.pipe = c => g => Cell.of(x => match({
-//   Stable: y => Stable.of(y),
-//   Recompute: (x) => {
-//     console.log('about to recompute', x)
-//     const [head, [_, tail]] = x
-//     return Recompute.of([
-//       head,
-//       g(Recompute.of([head, tail]))
-//     ])
-//   }
-// })(c(x)))
-
 Cell.product = c => g =>
   Cell.of(
     x => match({
@@ -73,13 +61,6 @@ const c = Cell( x => x.toUpperCase() )
 const b = Cell( x => `${x} of Cells` ).product(c)
 const a = Cell( x => `${x} world` ).product(b)
 
-// const result = a(Recompute.of(['hello', ['not expected', ['wat', ['HELLO WORLD OF CELLS', []]]]]))
-//
-// console.log(result)
-// console.log(result['@@value'])
-// console.log(result['@@value'][1]['@@value'])
-// console.log(result['@@value'][1]['@@value'][1]['@@value'])
-
 const result = a(Recompute.of(['hello', ['not expected', ['as', ['asd', ['adsf', []]]]]]))
 
 console.log(result)
@@ -87,95 +68,13 @@ console.log(result['@@value'])
 console.log(result['@@value'][1]['@@value'])
 console.log(result['@@value'][1]['@@value'][1]['@@value'])
 
-// const c = Cell( x => x.toUpperCase() )
+const fromRecomputation = x =>
+  (next => match({Stable: next, Recompute: next })(x))
+  (([head, tail]) => [head, tail != null ? fromRecomputation(tail) : null ])
 
-// console.log(b(Recompute.of(['world', ['world of Cells', []]])))
-
-// const result = a.pipe(b).pipe(c)(
-//   Recompute.of([
-//     'hello',
-//     [
-//       'hello monica',
-//       [
-//         'hello world of Cells',
-//         [
-//           '',
-//           [
-//             '',
-//             [
-//               '',
-//               []
-//             ]
-//           ]
-//         ]
-//       ]
-//     ]
-//   ])
-// )
-//
-// console.log(result)
-// console.log(result['@@value'])
-// console.log(result['@@value']['@@value'])
-
-// const tree = Recompute.of([2, Stable.of([4, Recompute.of([5, 0])])])
-//
-// const fromRecomputation = x =>
-//   (next => match({Stable: next, Recompute: next })(x))
-//   (([head, tail]) => [head, tail != null ? fromRecomputation(tail) : null ])
-//
-// console.log(tree)
-// console.log(fromRecomputation(tree))
-
-
-// const Cell = f => {
-//   const c = Arrow( either(
-//     ([head, tail]) => (
-//       result => (console.log('result', result, tail), result === tail[0]
-//         ? Right.of(tail)
-//         : Left.of([result, tail[1]]))
-//     )(Arrow(f)(head))
-//   )(
-//     Arrow(id).pipe(Left.of)
-//   ) )
-//
-//   c.pipe = g =>
-//     Cell.of(
-//       x =>
-//         either(
-//           ([head, tail]) => Left.of([
-//             head,
-//             g(
-//               Left.of(
-//                 (console.log('tail', tail), tail)
-//               )
-//             )
-//           ])
-//         )(
-//           Arrow(id).pipe(Right.of)
-//         )(c(x))
-//     )
-//
-//   return c
-// }
-//
-// Cell.of = f => Arrow(f)
-//
-// const b = Cell( x => (console.log('b', x), x + 2) )
-// const c = Cell.of( Arrow(id).product(id).sum(Arrow(id).product(id)) )
-//
-// const finalResult = a.pipe(id)(Left.of([2, [2, [5, [null]]]]))
-//
-// console.log('finalResult', finalResult)
-// console.log('finalResultInner', finalResult['@@value'])
-
-// console.log(
-//   a.pipe(b)(
-//     Left.of([
-//       2,
-//       [
-//         2,
-//         null
-//       ]
-//     ])
-//   )
-// )
+export {
+  Stable,
+  Recompute,
+  Cell,
+  fromRecomputation,
+}
