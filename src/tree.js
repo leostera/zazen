f351912b840add410e96530fbe212e41c6963e30
Xavier Via <fernando.via@gmail.com>
2017-02-26 03:01:25 +0100

    PoC mapping over special dual tree

--- src/tree.js
+++ src/tree.js
@@ -2,23 +2,47 @@ import {
   match,
 } from './cond'
 
-const inspect = x => x.inspect
-  ? x.inspect()
-  : x.toString()
+import {
+  Cell,
+} from './cell'
+
+import {
+  Right,
+  Left,
+  either,
+} from './either'
+
+import {
+  second,
+} from './pair'
+
+import {
+  inspect,
+} from './type'
+
+import {
+  blue,
+  green,
+  red,
+} from 'chalk'
 
 const Branch = (north, west, east) => ({
   '@@value': {north, west, east},
-  '@@type': Symbol.for('Branch'),
+  '@@type': 'Branch',
   inspect: () => `Branch(north: ${inspect(north)}, west: ${inspect(west)}, east: ${inspect(east)})`,
   fold: f => f(north),
   map: f => {
     const newBranch = f({
       north,
-      west: west['@@value'] ? west['@@value'].north : west,
-      east: east['@@value'] ? east['@@value'].north : east
+      west: west['@@type'] === 'Branch' || west['@@type'] === 'Leaf'
+        ? west['@@value'].north
+        : west,
+      east: east['@@type'] === 'Branch' || east['@@type'] === 'Leaf'
+        ? east['@@value'].north
+        : east
     })
 
-    const newWest = west['@@type']
+    const newWest = west['@@type'] === 'Branch' || west['@@type'] === 'Leaf'
       ? match({
         Branch: x => Branch(
           newBranch.west,
@@ -29,7 +53,7 @@ const Branch = (north, west, east) => ({
       })(west)
       : newBranch.west
 
-    const newEast = west['@@type']
+    const newEast = east['@@type'] === 'Branch' || east['@@type'] === 'Leaf'
       ? match({
         Branch: x => Branch(
           newBranch.east,
@@ -40,12 +64,14 @@ const Branch = (north, west, east) => ({
       })(east)
       : newBranch.east
 
+    console.log(green('newEast'), newEast)
+
     return Branch(
       newBranch.north,
-      newWest.map
+      west['@@type'] === 'Branch' || west['@@type'] === 'Leaf'
         ? newWest.map(f)
         : newWest,
-      newEast.map
+      east['@@type'] === 'Branch' || east['@@type'] === 'Leaf'
         ? newEast.map(f)
         : newEast
     )
@@ -54,28 +80,77 @@ const Branch = (north, west, east) => ({
 
 const Leaf = (north) => ({
   '@@value': {north},
-  '@@type': Symbol.for('Leaf'),
+  '@@type': 'Leaf',
   inspect: () => `Leaf(${north})`,
   fold: f => f(north),
   map: f => Leaf(f({north}).north)
 })
 
-const myTree = Branch(1, 2, 3)
+const myTree = Branch(
+  [Cell( x => x + 1 ), Right.of([100, 300])],
+  [Cell( x => x + 4 ), Right.of([200, 300])],
+  Branch(
+    [Cell( x => x * 2 ), Right.of([13, 22])],
+    [Cell( x => x * 7 ), Right.of([62, 64])],
+    [Cell( x => x / 2 ), Right.of([82, 84])]
+  )
+)
+
 // const myTree = Branch(
 //   1,
 //   Branch(3, 5, 4),
 //   Leaf(6)
 // )
 
-console.log(myTree)
+// console.log(myTree['@@value'].east)
 
-const tree2 = myTree.map(({north, west, east}) => ({
-  north: north,
-  west: north + west,
-  east: north + east
-}))
+const tree2 = myTree.map(({north, west, east}) => {
+  const northResult = north[0](north[1])
 
-console.log(tree2)
+  const argument = direction => [
+    either(second)(second)(northResult),
+    either(second)(second)(direction[1])
+  ]
+
+  const westResult = match({
+    Left: () => west[0](Left.of(argument(west))),
+    Right: () => west[0](Right.of(argument(west)))
+  })(north[1])
+
+  const eastResult = match({
+    Left: () => east[0](Left.of(argument(east))),
+    Right: () => east[0](Right.of(argument(east)))
+  })(north[1])
+
+  console.log(red('north'), north, northResult)
+  console.log(red('west'), west, westResult)
+  console.log(red('east'), east, eastResult)
+
+  return {
+    north: [north[0], northResult],
+    west: [west[0], westResult],
+    east: [east[0], eastResult]
+  }
+})
+
+const getResultTree = ({north, west, east}) => {
+  return ({
+    north: north instanceof Array ? north[1] : north,
+    east: east['@@type'] === 'Branch' || east['@@type'] === 'Leaf'
+      ? east.map(getResultTree)
+      : east[1],
+    west: west['@@type'] === 'Branch' || west['@@type'] === 'Leaf'
+      ? west.map(getResultTree)
+      : west[1],
+  })
+}
+
+const tree3 = tree2.map(getResultTree)
+
+console.log(tree3['@@value'])
+
+
+
 // Branch(
 //   1,
 //   Branch(4, 9, 8),
