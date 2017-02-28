29e7fb0a87646344a9c747bfa10cb8ecfdd07610
Leandro Ostera <leandro@ostera.io>
2017-02-28 22:03:06 +0100

    Full fat coverage for pair too

--- src/__tests__/pair.test.js
+++ src/__tests__/pair.test.js
@@ -1,14 +1,26 @@
 import {
+  Pair,
   swap,
   untag,
-} from 'zazen'
+  first,
+  second,
+} from '../pair'
 
 const a = [1,2]
 const b = [2,1]
 const c = ["what", 3]
 
-test("Can swap a pair",
+test(`Can create a Pair`,
+  () => expect( Pair(1,2) ).toEqual( [1,2] ))
+
+test(`first extracts the first element of a pair`,
+  () => expect( first(Pair(1,2)) ).toEqual( 1 ))
+
+test(`second extracts the second element of a pair`,
+  () => expect( second(Pair(1,2)) ).toEqual( 2 ))
+
+test(`Can swap a pair`,
   () => expect( swap(a) ).toEqual(b))
 
-test("Can untag a tagged pair",
+test(`Can untag a tagged pair`,
   () => expect( untag(c) ).toEqual(3))
