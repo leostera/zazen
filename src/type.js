f7f8c3bfc35be8114c88132aabf881b7003843f8
Leandro Ostera <leandro@ostera.io>
2017-03-18 17:32:02 +0100

    Let inspect helper handle Nil values

--- src/type.js
+++ src/type.js
@@ -1,5 +1,6 @@
 import {
   cond,
+  isNil,
 } from './prelude'
 
 /*
@@ -56,6 +57,7 @@ export type Empty<A> = A
 const inspect = (a: Object): string => {
   if( a instanceof Array )  return `[${a.map(inspect)}]`
   if( a instanceof Object ) return JSON.stringify(a)
+  if( isNil(a) ) return "Nil"
   return a.toString()
 }
 
